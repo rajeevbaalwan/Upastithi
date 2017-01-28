1a7403239787df543e3303c61aa97d60ac011d78
rajeevyadav <rajeevy899@gmail.com>
2017-01-28 19:04:34 +0530

    Spinners added

--- app/src/main/java/in/evolve/upastithi/Student/Fragments/TabQueries.java
+++ app/src/main/java/in/evolve/upastithi/Student/Fragments/TabQueries.java
@@ -1,19 +1,33 @@
 package in.evolve.upastithi.Student.Fragments;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import in.evolve.upastithi.R;
+import in.evolve.upastithi.Student.Activities.StudentChatActivity;
 
 /**
  * Created by RAJEEV YADAV on 10/27/2016.
  */
 public class TabQueries extends Fragment {
 
+    private Button queryButton;
     public TabQueries() {
     }
 
@@ -26,6 +40,58 @@ public class TabQueries extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_pager_student_queires,container,false);
+        Spinner spinner= (Spinner) fragmentView.findViewById(R.id.student_query_teacher_select_spinner);
+        queryButton= (Button) fragmentView.findViewById(R.id.make_query_button);
+        Resources resources=getResources();
+
+        String[] teacher=resources.getStringArray(R.array.teacher_names);
+        List<String> list=new ArrayList<>(Arrays.asList(teacher));
+
+        ArrayAdapter<String> spinnerArrayAdapter=new ArrayAdapter<String>(getActivity(),
+                                        R.layout.spinner_single_item,list){
+
+            @Override
+            public boolean isEnabled(int position) {
+                if(position==0)
+                    return false;
+                else
+                    return  true;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view= super.getDropDownView(position, convertView, parent);
+                TextView textView= (TextView) view;
+
+                if(position%2==1)
+                    textView.setBackgroundColor(Color.parseColor("#ffffff"));
+                else textView.setBackgroundColor(Color.parseColor("#eceff7"));
+                return view;
+            }
+        };
+
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_single_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),StudentChatActivity.class);
+                startActivity(intent);
+            }
+        });
         return fragmentView;
     }
 }
--- app/src/main/java/in/evolve/upastithi/Teacher/Activities/EditMarksActivity.java
+++ app/src/main/java/in/evolve/upastithi/Teacher/Activities/EditMarksActivity.java
@@ -1,15 +1,166 @@
 package in.evolve.upastithi.Teacher.Activities;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import in.evolve.upastithi.R;
 
 public class EditMarksActivity extends AppCompatActivity {
 
+    private Spinner courseSpinner;
+    private Spinner departmentSpinner;
+    private Spinner yearSpinner;
+    private Button editMarks;
+    private String[] course;
+    private String[] department;
+    private String[] year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_marks);
+
+        courseSpinner= (Spinner) findViewById(R.id.teacher_select_course_spinner);
+        departmentSpinner= (Spinner) findViewById(R.id.teacher_select_department_spinner);
+        yearSpinner= (Spinner) findViewById(R.id.teacher_select_year_spinner);
+        editMarks= (Button) findViewById(R.id.edit_marks_button);
+
+        Resources resources=getResources();
+        course=resources.getStringArray(R.array.courses);
+        department=resources.getStringArray(R.array.department);
+        year=resources.getStringArray(R.array.course_year);
+
+        List<String> courseList=new ArrayList<>(Arrays.asList(course));
+        List<String> departmentList=new ArrayList<>(Arrays.asList(department));
+        List<String> yearList=new ArrayList<>(Arrays.asList(year));
+
+        ArrayAdapter<String> courseAdapter=new ArrayAdapter<String>(this,
+                R.layout.spinner_single_item,courseList){
+
+            @Override
+            public boolean isEnabled(int position) {
+                if(position==0)
+                    return false;
+                else
+                    return  true;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view= super.getDropDownView(position, convertView, parent);
+                TextView textView= (TextView) view;
+
+                if(position%2==1)
+                    textView.setBackgroundColor(Color.parseColor("#ffffff"));
+                else textView.setBackgroundColor(Color.parseColor("#eceff7"));
+                return view;
+            }
+        };
+
+        courseAdapter.setDropDownViewResource(R.layout.spinner_single_item);
+        courseSpinner.setAdapter(courseAdapter);
+
+        courseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        ArrayAdapter<String> departmentAdapter=new ArrayAdapter<String>(this,
+                R.layout.spinner_single_item,departmentList){
+
+            @Override
+            public boolean isEnabled(int position) {
+                if(position==0)
+                    return false;
+                else
+                    return  true;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view= super.getDropDownView(position, convertView, parent);
+                TextView textView= (TextView) view;
+
+                if(position%2==1)
+                    textView.setBackgroundColor(Color.parseColor("#ffffff"));
+                else textView.setBackgroundColor(Color.parseColor("#eceff7"));
+                return view;
+            }
+        };
+
+        departmentAdapter.setDropDownViewResource(R.layout.spinner_single_item);
+        departmentSpinner.setAdapter(departmentAdapter);
+
+        departmentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        ArrayAdapter<String> yearAdapter=new ArrayAdapter<String>(this,
+                R.layout.spinner_single_item,yearList){
+
+            @Override
+            public boolean isEnabled(int position) {
+                if(position==0)
+                    return false;
+                else
+                    return  true;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view= super.getDropDownView(position, convertView, parent);
+                TextView textView= (TextView) view;
+
+                if(position%2==1)
+                    textView.setBackgroundColor(Color.parseColor("#ffffff"));
+                else textView.setBackgroundColor(Color.parseColor("#eceff7"));
+                return view;
+            }
+        };
+
+        yearAdapter.setDropDownViewResource(R.layout.spinner_single_item);
+        yearSpinner.setAdapter(yearAdapter);
+
+        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
